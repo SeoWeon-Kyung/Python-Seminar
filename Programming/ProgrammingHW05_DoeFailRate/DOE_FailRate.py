8bd661968526e065d482156ad3c58f089a51127d
SeoWeon-Kyung <rudtjdnjs@naver.com>
2021-06-09 12:23:56 +0900

    8th HW midldle end

--- Programming/ProgrammingHW05_DoeFailRate/DOE_FailRate.py
+++ Programming/ProgrammingHW05_DoeFailRate/DOE_FailRate.py
@@ -22,8 +22,8 @@ def spec_check(specarr, dataarr):
     dataarr = dataarr[test_names].astype('float')
     checked_arr = (specarr.loc[test_names, 'LOWER_LIMIT'] > dataarr[test_names]) \
         | (specarr.loc[test_names, 'UPPER_LIMIT'] < dataarr[test_names])
-    checked_arr['error sum'] = checked_arr.sum(1)
-    specout = checked_arr[checked_arr['error sum'] != 0]
+    checked_arr['error_sum'] = checked_arr.sum(1)
+    specout = checked_arr[checked_arr['error_sum'] != 0]
 
     return specout
 
@@ -60,9 +60,13 @@ for k in AA_files.keys():
     AA_data = read_log(folderpath, AA_files[k], 'AA')
     EOL_data = read_log(folderpath, EOL_files[k], 'EOL')
     EOL_data['moduleConfig'] = k
-    log[k] = pd.merge(AA_data, EOL_data, how='inner', left_on='SensorID', right_on='Tx_ASIC_ID', on='site')
+    log[k] = pd.merge(AA_data, EOL_data, how='inner', left_on='SensorID', right_on='Tx_ASIC_ID', suffixes=('_AA', '_EOL'))
     log[k].set_index('Tx_ASIC_ID', drop=False, inplace=True)
 
+# config 별 데이터를 위아래로 하나로 합치기 - 위의 for 문에 넣어도 됨
+data = pd.DataFrame()
+for k in log.keys():
+    data = pd.concat([data, log[k]])
 
 # merge 한 data의 불량률 계산 
 # spec data 받아오기 
@@ -78,36 +82,33 @@ p_sfr60 = re.compile(r'sfr_60cm')
 spec_sfr20 = spec.loc[[col for col in spec.index if p_sfr20.search(col)]]
 spec_sfr60 = spec.loc[[col for col in spec.index if p_sfr60.search(col)]]
 
-# spec limit과 Data 비교해서 spec out 인것만 True로 쓰기 
-C4004specout_sfr20 = spec_check(spec_sfr20, log['C4004'])
-C4004specout_sfr60 = spec_check(spec_sfr60, log['C4004'])
-#C4004_sfr20_specout = C4004spec_sfr20.sum(1)[C4004spec_sfr20.sum(1) != 0]
-#C4004_sfr60_specout = C4004spec_sfr60.sum(1)[C4004spec_sfr60.sum(1) != 0]
+# spec limit과 Data 비교해서 spec out Data가 있는 index만 골라내기
+specout_sfr20 = spec_check(spec_sfr20, data)
+specout_sfr60 = spec_check(spec_sfr60, data)
 
-#print(len(C4004specout_sfr20.index))
 """
 count를 만들어서 에러가 있으면 1을 넣는다. 
 마찬가지로 sfr20 에는 거기만 에러가 난걸 넣는다.
 sfr60에는 거기만. 
 sfr 2060에는 둘다 난 애들만 넣는다. 
 """
-"""
-C4004specout_both = pd.merge(C4004specout_sfr20, C4004specout_sfr60, how='inner', \
-    left_index=True, right_index=True, suffixes=('_sfr20', '_sfr60'))
-print(C4004specout_both)
-C4004specout_exc20 = C4004specout_sfr20.loc[C4004specout_both.index]
+# fail data 수 세기 위한 결과물 sheet 만들기
+fail_data = pd.DataFrame(index=data.index)
+fail_data = data[['moduleConfig', 'site_AA', 'Offset_Z']].copy()
+new_col = ['count', 'cm20', 'cm60', 'cm2060']
+for col in new_col: fail_data[col] = 0
 
-print(C4004specout_exc20)
-"""
-fail_data = pd.DataFrame(index=log['C4004'].index)
-#print(log['C4004'].columns)
-fail_data = log['C4004']['site'].copy()
-print(fail_data.head())
+# fail data 를 sfr20, 60, 2060 exclusive 하게 만듬
+fail_data[new_col[1]][specout_sfr20.index] = 1
+fail_data[new_col[2]][specout_sfr60.index] = 1
+fail_data[new_col[3]] = fail_data[new_col[1]] * fail_data[new_col[2]]
+for col in new_col[1:3]: fail_data[col] -= fail_data[new_col[-1]]
+for col in new_col[1:]: fail_data[new_col[0]] += fail_data[col]
 
+# Groupby
+fail_rate_result = fail_data.groupby(['moduleConfig', 'site_AA', 'Offset_Z']).sum()
+for col in new_col[1:]: fail_rate_result[col] = fail_rate_result[col] / fail_rate_result[new_col[0]] * 100
 
+# result 출력
+fail_rate_result.to_csv(folderpath+ '/hw5_result.csv')
 
-
-
-#test_sfr20 = spec_data['SUMMARY_LOG'][lambda indx: p_sfr20.search(indx)]
-#print(test_sfr20)
-#spec_data.loc(,)
